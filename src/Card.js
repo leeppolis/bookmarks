39711eba95a9500692d0a00060c9dd3fcdd0cc13
Simone Lippolis <Simone.lippolis@frogdesign.com>
2017-09-28 18:21:55 +0200

    Added check on # of tags

--- src/Card.js
+++ src/Card.js
@@ -3,44 +3,48 @@ import "./Card.css";
 import Button from "./Button.js";
 
 class Card extends Component {
-	constructor(props) {
-    	super(props);
-		this.checkImage = this.checkImage.bind(this);
-	}
-
-  checkImage() {
-      if ( this.props.fields.media && this.props.fields.media !== null && this.props.fields.media !== undefined && this.props.fields.media.length > 0 ) {
-          return <a href={this.props.fields.link}><img src={this.props.fields.media} alt={this.props.fields.title} /></a>;
-      } else {
-          return '';
-      }
-  }
-
-  checkDescription() {
-    if ( this.props.fields.content && this.props.fields.content !== null && this.props.fields.content !== undefined && this.props.fields.content.length > 0 ) {
-        return <p><a href={this.props.fields.link}>{this.props.fields.content}</a></p>;
-    } else {
-        return '';
+    constructor(props) {
+        super(props);
+        this.showMore = this.showMore.bind(this);
+        this.state = { showAllTags: false };
     }
+    
+  showMore() {
+    this.setState( { showAllTags: !this.state.showAllTags } );
   }
 
   render() {
+    let i = 0;
     return (
 		<article className="card">
             <header>
                 <h1><a href={this.props.fields.link}>{this.props.fields.title}</a></h1>
             </header>
             <section>
-                {this.checkImage()}
-                {this.checkDescription()}
+                {this.props.fields.media && <a href={this.props.fields.link}><img src={this.props.fields.media} alt={this.props.fields.title} /></a>}
+                {this.props.fields.content && <p><a href={this.props.fields.link}>{this.props.fields.content}</a></p>}
             </section>
             <footer>
-                <ul>
+                <ul className={ this.state.showAllTags ? 'show-all' : 'show-default'}>
                     <li>Tags</li>
-                {
+                {   
                     this.props.fields.tags.map(
                         function(tag) {
-                            return <li key={tag}><Button value={tag} clicked={this.props.quickSearch} label={'#' + tag} variant="tag" /></li>
+                            if (i < 4) {
+                                i++;
+                                return <li key={tag}><Button value={tag} clicked={this.props.quickSearch} label={'#' + tag} variant="tag" /></li>;
+                            } else if (i === 4 && this.props.fields.tags.length <= 5) {
+                                i++;
+                                return <li key={tag}><Button value={tag} clicked={this.props.quickSearch} label={'#' + tag} variant="tag" /></li>;
+                            } else if ( i === 4) {
+                                i++;
+                                let remaining = (this.props.fields.tags.length - 5);
+                                remaining = ( remaining < 11 ) ? remaining : '10+';
+                                return <li className="with-switch" key={tag}><Button value={tag} clicked={this.props.quickSearch} label={'#' + tag} variant="tag" /> <Button clicked={this.showMore} label={remaining} variant="more" /></li>;
+                            } else {
+                                i++;
+                                return <li className="additional" key={tag}><Button value={tag} clicked={this.props.quickSearch} label={'#' + tag} variant="tag" /></li>;
+                            }
                         },
                         this
                     )
